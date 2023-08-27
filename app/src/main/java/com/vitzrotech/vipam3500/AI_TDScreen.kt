98ef481f12d5e2c50dfd1f90948f56c1e598d4f8
Cabbage-shaved-ice <142379337+ice-cabbage@users.noreply.github.com>
2023-08-27 13:10:13 +0900

    Analog TD input

--- app/src/main/java/com/vitzrotech/vipam3500/AI_TDScreen.kt
+++ app/src/main/java/com/vitzrotech/vipam3500/AI_TDScreen.kt
@@ -18,9 +18,59 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.vitzrotech.vipam3500.ui.theme.VIPAM3500Theme
 
+data class Input (
+    val name: String = "",
+    val ma: Float = 0.0f,
+    val temp: String = ""
+)
+
 @Composable
 fun AI_TDScreen(viewModel: SharedViewModel) {
+    val ma1 by remember { viewModel.ma1 }
+    val ma2 by remember { viewModel.ma2 }
+    val ma3 by remember { viewModel.ma3 }
+    val ma4 by remember { viewModel.ma4 }
 
+    val analog = arrayOf(
+        Input("AI 1", ma1, "-"),
+        Input("AI 2", ma2, "-"),
+        Input("AI 3", ma3, "-"),
+        Input("AI 4", ma4, "-")
+    )
+
+    LazyColumn(
+        Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+            .border(1.dp, Color.Black)) {
+        items(analog.size) {
+            val v = analog[it]
+            Row(Modifier.fillMaxWidth()) {
+                Text(v.name,
+                    Modifier
+                        .weight(0.4f)
+                        .height(40.dp)
+                        .border(0.6.dp, Color.Black),
+                    textAlign = TextAlign.Center
+                )
+                Text(
+                    "${v.ma.toUnitString("%.02f", v.temp)}",
+                    Modifier
+                        .weight(0.3f)
+                        .height(40.dp)
+                        .border(0.6.dp, Color.Black),
+                    textAlign = TextAlign.Center
+                )
+                Text(v.temp,
+                    Modifier
+                        .weight(0.2f)
+                        .height(40.dp)
+                        .border(0.6.dp, Color.Black),
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+    }
 }
 
 @Preview

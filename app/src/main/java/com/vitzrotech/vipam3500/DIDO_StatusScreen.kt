9ade170f8c98d16a4a9f1470921f4972a812e57e
Cabbage-shaved-ice <142379337+ice-cabbage@users.noreply.github.com>
2023-09-07 15:45:25 +0900

    DI/DO Screen

--- app/src/main/java/com/vitzrotech/vipam3500/DIDO_StatusScreen.kt
+++ app/src/main/java/com/vitzrotech/vipam3500/DIDO_StatusScreen.kt
@@ -1,13 +1,23 @@
 package com.vitzrotech.vipam3500
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.vitzrotech.vipam3500.ui.theme.VIPAM3500Theme
 
+
+data class DIDO (
+    val value: Float = 0.0f
+)
+
 @Composable
 fun DIDO_StatusScreen(viewModel: SharedViewModel) {
-
+    val diStatus2 by remember { viewModel.diStatus2 }
+    val diStatus1 by remember { viewModel.diStatus1 }
+    val doStatus2 by remember { viewModel.doStatus2 }
+    val doStatus1 by remember { viewModel.doStatus1 }
 }
 
 @Preview

f8968b184297b6154dbbe512cef0c66d9f23e357
Cabbage-shaved-ice <142379337+ice-cabbage@users.noreply.github.com>
2023-09-11 11:37:58 +0900

    LocalScreen
    dialog

--- app/src/main/java/com/vitzrotech/vipam3500/LocalScreen.kt
+++ app/src/main/java/com/vitzrotech/vipam3500/LocalScreen.kt
@@ -1,18 +1,29 @@
 package com.vitzrotech.vipam3500
 
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
 import com.vitzrotech.vipam3500.ui.theme.VIPAM3500Theme
 
 @Composable
-fun LocalScreen() {
+fun LocalScreen(password: String, onDismiss: () -> Unit, onOK: () -> Unit) {
+    var typed by remember { mutableStateOf("") }
+    var visible by remember { mutableStateOf(false) }
+    Dialog(onDismissRequest = onDismiss) {
+        Surface (
 
+        )
+    }
 }
 
 @Composable
 @Preview
 fun LocalScreenPreview() {
     VIPAM3500Theme {
-        LocalScreen()
+        LocalScreen("", onDismiss = {}) {}
     }
 }

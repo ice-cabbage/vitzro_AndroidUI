1ecdd9b9a9e7888ce1e98487e3ac9ec5b0dab653
Cabbage-shaved-ice <142379337+ice-cabbage@users.noreply.github.com>
2023-09-11 13:37:50 +0900

    LocalScreen
    password dialog

--- app/src/main/java/com/vitzrotech/vipam3500/MainActivity.kt
+++ app/src/main/java/com/vitzrotech/vipam3500/MainActivity.kt
@@ -252,9 +252,6 @@ fun NavGraphBuilder.mainGraph(navController: NavHostController, viewModel: Share
         composable("Company") {
             CompanyScreen(viewModel)
         }
-        composable("Local") {
-            LocalScreen()
-        }
     }
 }
 

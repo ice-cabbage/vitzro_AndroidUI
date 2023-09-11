5a9a07ff42ebdc3feec98504a2a644b945ac92a3
Cabbage-shaved-ice <142379337+ice-cabbage@users.noreply.github.com>
2023-09-11 10:57:13 +0900

    viewModel
    import SharedViewModel

--- app/src/main/java/com/vitzrotech/vipam3500/CompanyScreen.kt
+++ app/src/main/java/com/vitzrotech/vipam3500/CompanyScreen.kt
@@ -2,12 +2,13 @@ package com.vitzrotech.vipam3500
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.vitzrotech.vipam3500.ui.theme.VIPAM3500Theme
 
 @Composable
-fun CompanyScreen(navController: NavHostController) {
+fun CompanyScreen(viewModel: SharedViewModel) {
 
 }
 
@@ -15,6 +16,6 @@ fun CompanyScreen(navController: NavHostController) {
 @Preview
 fun CompanyScreenPreview() {
     VIPAM3500Theme {
-        CompanyScreen(rememberNavController())
+        CompanyScreen(viewModel())
     }
 }
--- app/src/main/java/com/vitzrotech/vipam3500/Simulation_TestScreen.kt
+++ app/src/main/java/com/vitzrotech/vipam3500/Simulation_TestScreen.kt
@@ -2,13 +2,14 @@ package com.vitzrotech.vipam3500
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.vitzrotech.vipam3500.ui.theme.VIPAM3500Theme
 
 @Composable
-fun Simulation_TestScreen(navController: NavHostController) {
+fun Simulation_TestScreen(viewModel: SharedViewModel) {
 
 }
 
@@ -16,6 +17,6 @@ fun Simulation_TestScreen(navController: NavHostController) {
 @Preview
 fun Simulation_TestScreenPreview() {
     VIPAM3500Theme {
-        Simulation_TestScreen(rememberNavController())
+        Simulation_TestScreen(viewModel())
     }
 }

14b3fab65f8295b55649aade97152e1bb63bdb25
Cabbage-shaved-ice <142379337+ice-cabbage@users.noreply.github.com>
2023-09-11 11:18:00 +0900

    import edit

--- app/src/main/java/com/vitzrotech/vipam3500/CompanyScreen.kt
+++ app/src/main/java/com/vitzrotech/vipam3500/CompanyScreen.kt
@@ -3,8 +3,6 @@ package com.vitzrotech.vipam3500
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
 import com.vitzrotech.vipam3500.ui.theme.VIPAM3500Theme
 
 @Composable

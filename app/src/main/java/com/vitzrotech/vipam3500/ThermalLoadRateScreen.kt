9e3a44d78373f7a33c31fc9d01d43df0cc12c8c6
Cabbage-shaved-ice <142379337+ice-cabbage@users.noreply.github.com>
2023-09-20 10:42:51 +0900

    ThermalLoadRateScreen Done

--- app/src/main/java/com/vitzrotech/vipam3500/ThermalLoadRateScreen.kt
+++ app/src/main/java/com/vitzrotech/vipam3500/ThermalLoadRateScreen.kt
@@ -18,9 +18,50 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.vitzrotech.vipam3500.ui.theme.VIPAM3500Theme
 
+data class LoadRate (
+    val name: String = "",
+    val per: Float = 0.0f,
+    val unit: String = ""
+)
+
 @Composable
 fun ThermalLoadRateScreen(viewModel: SharedViewModel) {
+    val lev by remember { viewModel.lev }
+    val ia by remember { viewModel.ia }
+    val ib by remember { viewModel.ib }
+    val ic by remember { viewModel.ic }
 
+    val theraml = arrayOf(
+        LoadRate("열상태", lev, "%"),
+        LoadRate("부하율 전류 A상", ia, "%"),
+        LoadRate("부하율 전류 B상", ib, "%"),
+        LoadRate("부하율 전류 C상", ic, "%")
+    )
+    LazyColumn(
+        Modifier
+            .fillMaxWidth()
+            .padding()) {
+        items(theraml.size) {
+            val v = theraml[it]
+            Row(Modifier.fillMaxWidth()) {
+                Text(v.name,
+                    Modifier
+                        .weight(0.3f)
+                        .height(40.dp)
+                        .border(0.6.dp, Color.Black),
+                    textAlign = TextAlign.Center
+                )
+                Text(
+                    "${v.per.toUnitString("%.02f", v.unit)}",
+                    Modifier
+                        .weight(0.2f)
+                        .height(40.dp)
+                        .border(0.6.dp, Color.Black),
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+    }
 }
 
 @Preview

eef30c0fd828a036a0099707bce8f326baf11655
Cabbage-shaved-ice <142379337+ice-cabbage@users.noreply.github.com>
2023-09-21 14:42:13 +0900

    add AboutScreen

---
+++ app/src/main/java/com/vitzrotech/vipam3500/AboutScreen.kt
@@ -0,0 +1,38 @@
+package com.vitzrotech.vipam3500
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.vitzrotech.vipam3500.ui.theme.VIPAM3500Theme
+
+@Composable
+fun AboutScreen() {
+    LazyColumn (
+        Modifier
+            .fillMaxWidth()
+            .padding(16.dp)) {
+        Row(Modifier.fillMaxWidth()) {
+            Text("VIPAM 3500")
+        }
+    }
+}
+
+@Composable
+@Preview
+fun AboutScreenPreview() {
+    VIPAM3500Theme {
+        AboutScreen()
+    }
+}

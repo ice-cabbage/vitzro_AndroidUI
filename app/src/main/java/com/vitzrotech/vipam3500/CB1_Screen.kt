ab476864de2e7343a972d04069d484c9b4b0f4e9
Cabbage-shaved-ice <142379337+ice-cabbage@users.noreply.github.com>
2023-09-22 14:24:08 +0900

    CB1 Screen

--- app/src/main/java/com/vitzrotech/vipam3500/CB1_Screen.kt
+++ app/src/main/java/com/vitzrotech/vipam3500/CB1_Screen.kt
@@ -1,13 +1,114 @@
 package com.vitzrotech.vipam3500
 
+import android.content.Context
+import android.content.res.Configuration.ORIENTATION_PORTRAIT
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import android.widget.SeekBar
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.navigation
+import androidx.navigation.compose.rememberNavController
 import com.vitzrotech.vipam3500.ui.theme.VIPAM3500Theme
+import kotlinx.coroutines.launch
+import android.widget.TextView
+import androidx.compose.foundation.layout.fillMaxWidth
 
 @Composable
 fun CB1_Screen(viewModel: SharedViewModel) {
+    val cb1 by remember { viewModel.cb1 }
+    //var cb1Clicked by remember { mutableListOf(false) }
+    val cb1Color = if (cb1 == 1u) Color(0xFF00CC00) else if (cb1 == 2u) Color(0xFFFFF0000) else Color(0xFF5A5A5A)
 
+    //if (cb1Clicked) {
+        //PasswordDialog("0000", onDismiss = { cb1Clicked = false }) {
+            //Toast.makeText(context, "CB1 clicked", Toast.LENGTH_SHORT).show()
+        //}
+    //}
+    //Column (
+        //modifier = Modifier
+            //.fillMaxWidth()
+            //.padding(
+                //top = it.calculateTopPadding(),
+                //bottom = it.calculateBottomPadding()
+            //)
+            //.background(MaterialTheme.colorScheme.background)
+            //.wrapContentSize(Alignment.Center),
+        //verticalArrangement = Arrangement.Center,
+        //horizontalAlignment = Alignment.CenterHorizontally
+    //) {
+        //NavHost(
+            //navController,
+            //startDestination = NavRoutes.MainRoute.name
+        //) {
+            //mainGraph(navController, viewModel)
+        //}
+    //}
 }
 
 @Composable

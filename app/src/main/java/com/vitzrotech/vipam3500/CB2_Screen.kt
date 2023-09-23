56e02490cead2853a0233428a0d08cf0919e39e5
Cabbage-shaved-ice <142379337+ice-cabbage@users.noreply.github.com>
2023-09-23 23:20:35 +0900

    CB2

--- app/src/main/java/com/vitzrotech/vipam3500/CB2_Screen.kt
+++ app/src/main/java/com/vitzrotech/vipam3500/CB2_Screen.kt
@@ -1,12 +1,22 @@
 package com.vitzrotech.vipam3500
 
 import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
 import com.vitzrotech.vipam3500.ui.theme.VIPAM3500Theme
 
 @Composable
@@ -18,6 +28,25 @@ fun CB2_Screen(viewModel: SharedViewModel) {
     if (cb2Clicked) {
         PasswordDialog("0000", onDismiss = { cb2Clicked = false }) {
             Toast.makeText(context, "CB1 clicked", Toast.LENGTH_SHORT).show()
+        }
+    }
+    Column (
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(
+                top = it.calculateTopPadding(),
+                bottom = it.calculateBottomPadding()
+            )
+            .background(MaterialTheme.colorScheme.background)
+            .wrapContentSize(Alignment.Center),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        NavHost(
+            navController,
+            startDestination = NavRoutes.MainRoute.name
+        ) {
+            mainGraph(navController, viewModel)
         }
     }
 }

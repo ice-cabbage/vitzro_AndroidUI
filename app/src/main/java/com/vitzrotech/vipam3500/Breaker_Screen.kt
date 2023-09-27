2038c56c5d92d1138b2c6d972a452c36b90b52a1
Cabbage-shaved-ice <142379337+ice-cabbage@users.noreply.github.com>
2023-09-27 15:08:11 +0900

    breaker failure screen
    in progress(data class)

--- app/src/main/java/com/vitzrotech/vipam3500/Breaker_Screen.kt
+++ app/src/main/java/com/vitzrotech/vipam3500/Breaker_Screen.kt
@@ -5,9 +5,19 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.vitzrotech.vipam3500.ui.theme.VIPAM3500Theme
 
+data class Breaker (
+    val name: String = "",
+    val des: String = ""
+)
+
+data class aker (
+    val na: String = "",
+    val descr: Float = 0.0f
+)
+
 @Composable
 fun Breaker_Screen(viewModel: SharedViewModel) {
-
+    
 }
 
 @Composable

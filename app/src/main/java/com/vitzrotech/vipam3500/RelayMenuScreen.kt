672a680c54a338400276db11428c6ba4ee5897db
Cabbage-shaved-ice <142379337+ice-cabbage@users.noreply.github.com>
2023-10-04 13:05:58 +0900

    add RelayTableTextRow, RelayTableText

--- app/src/main/java/com/vitzrotech/vipam3500/RelayMenuScreen.kt
+++ app/src/main/java/com/vitzrotech/vipam3500/RelayMenuScreen.kt
@@ -2,12 +2,14 @@ package com.vitzrotech.vipam3500
 
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -24,6 +26,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 data class OCRState(
@@ -243,9 +246,27 @@ fun RelayTable(captions: List<String>, stubs: List<String>, relays: List<List<An
             .fillMaxWidth()
             .border(2.dp, MaterialTheme.colorScheme.background)) {
         item {
-            //RelayTableTextRow("", captions)
+            RelayTableTextRow("", captions)
         }
     }
+}
+
+@Composable
+fun RelayTableTextRow(label: String, items: List<String>) {
+    Row(Modifier.height(IntrinsicSize.Min)) {
+        RelayTableText(label, Modifier.weight(1.0f))
+        items.forEach { RelayTableText(it, Modifier.weight(1.0f)) }
+    }
+}
+
+@Composable
+fun RelayTableText(text: String, modifier: Modifier) {
+    Text(text,
+        modifier
+            .border(1.dp, MaterialTheme.colorScheme.background)
+            .padding(8.dp, 4.dp)
+            .fillMaxWidth()
+            .wrapContentHeight(), textAlign = TextAlign.Center, color = MaterialTheme.colorScheme.onBackground)
 }
 
 fun isRelayInRange(items: List<List<Any?>>, relays: List<List<Any>>): Boolean {

f539904a213fd029ec9aa1f3f2b57357a61f3c51
Cabbage-shaved-ice <142379337+ice-cabbage@users.noreply.github.com>
2023-10-05 10:24:24 +0900

    Virtual DI Screen Done
    Button List

--- app/src/main/java/com/vitzrotech/vipam3500/VirtualDI_Screen.kt
+++ app/src/main/java/com/vitzrotech/vipam3500/VirtualDI_Screen.kt
@@ -1,19 +1,78 @@
 package com.vitzrotech.vipam3500
 
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.vitzrotech.vipam3500.ui.theme.VIPAM3500Theme
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.navigation.NavController
+
+data class ButtonTable(
+    val name: String = "",
+    val onOff: String = ""
+)
 
 @Composable
-fun VirtualDI_Screen(viewModel: SharedViewModel) {
+fun VirtualDI_Screen(navController: NavController) {
+    val buttonList = arrayListOf(
+        ButtonTable("Interlock Relieve 1", "Interlock Relieve 1"),
+        ButtonTable("Virtual DI 2", "Virtual DI 2"),
+        ButtonTable("Virtual DI 3", "Virtual DI 3"),
+        ButtonTable("Virtual DI 4", "Virtual DI 4"),
+        ButtonTable("Virtual DI 5", "Virtual DI 5"),
+        ButtonTable("Virtual DI 6", "Virtual DI 6"),
+        ButtonTable("Virtual DI 7", "Virtual DI 7"),
+        ButtonTable("Virtual DI 8", "Virtual DI 8")
+    )
+    val gridItems = buttonList.chunked(3)
 
+    LazyColumn {
+        items(gridItems) {rowItems ->
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                for (item in rowItems) {
+                    val modifier = Modifier
+                        .height(75.dp)
+                        .width(100.dp)
+                        .weight(1f)
+                        .padding(10.dp)
+
+                    Button(
+                        onClick = {
+                            navController.navigate(item.onOff) {
+                                popUpTo(popUpToId)
+                            }
+                        },
+                        modifier = modifier,
+                        shape = androidx.compose.ui.graphics.RectangleShape
+                    ) {
+                        Text(text = item.name)
+                    }
+                }
+            }
+        }
+    }
 }
 
 @Composable
 @Preview
 fun VirtualDI_ScreenPreview() {
     VIPAM3500Theme {
-        VirtualDI_Screen(viewModel())
+        VirtualDI_Screen(rememberNavController())
     }
 }

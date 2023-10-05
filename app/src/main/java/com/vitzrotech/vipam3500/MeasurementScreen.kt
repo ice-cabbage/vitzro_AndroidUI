913ed99e3c40790e75f47987fd2afdbe47c5747c
Cabbage-shaved-ice <142379337+ice-cabbage@users.noreply.github.com>
2023-10-05 15:54:47 +0900

    Button Menu Screen
    titles

--- app/src/main/java/com/vitzrotech/vipam3500/MeasurementScreen.kt
+++ app/src/main/java/com/vitzrotech/vipam3500/MeasurementScreen.kt
@@ -1,75 +1,76 @@
 package com.vitzrotech.vipam3500
 
-import androidx.compose.foundation.layout.Arrangement
+import android.content.res.Configuration
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavHostController
+import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.vitzrotech.vipam3500.ui.theme.VIPAM3500Theme
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 
-data class MenuItem(
-    val buttonName : String = "",
-    val dst : String = ""
-)
+val measurementTitles = mapOf(
+    "voltage/current" to "OCR (50/51)",
+    "power" to "OCGR (50G/51G)",
+    "energy" to "OVGR (64G)",
+    "reverse energy" to "SGR (67G)",
+    "di/do status" to "DGR (67N)",
+    "ai td" to "OVR (59)",
+    "thermal/loadrate" to "UVR (27)",
+    "sequence" to "POR (47P)",
+    "phase balance" to "NSOVR (47N)",
+    "harmonics" to "DOCR (67P)",
+    "k-factor/c-factor" to "Reclosing (79)",
+    "demand" to "Sync (25)",
+    "max/min" to "NSOCR (46)",
+    "true rms" to "Inrush (68)",
+    "relay status" to "UFR (81U)",
+    "interlock status" to "OFR (81O)",
+    "plc io memory" to "Active Power (32P)")
 
 @Composable
-fun MeasurementScreen(navController: NavHostController) {
-    val buttonList = arrayListOf(
-        MenuItem("Voltage current", "voltage_current"),
-        MenuItem("Power", "power"),
-        MenuItem("Energy", "energy"),
-        MenuItem("Reverse Energy", "reverse_Energy"),
-        MenuItem("DI/DO Status", "DI/DO_Status"),
-        MenuItem("AI TD", "AI_TD"),
-        MenuItem("Thermal/LoadRate", "Thermal/LoadRate"),
-        MenuItem("Sequence", "sequence"),
-        MenuItem("Phase Balance", "phase_Balance"),
-        MenuItem("Harmonics", "harmonics"),
-        MenuItem("K-factor/C-factor", "k-factor/c-factor"),
-        MenuItem("Demand", "demand"),
-        MenuItem("Max/Min", "max/min"),
-        MenuItem("True RMS", "true_RMS"),
-        MenuItem("Relay Status", "relay_Status"),
-        MenuItem("Interlock Status", "interlock_Status"),
-        MenuItem("PLC IO Memory", "PLC_IO_Memory"),
-        MenuItem("Oscilloscope", "oscilloscope")
-    )
-    val gridItems = buttonList.chunked(3)
+fun NaviButton(navController: NavController, route: String, modifier: Modifier) {
+    Button(onClick = {
+        navController.navigate(route) {
+            popUpTo(NavRoutes.MainRoute.name)
+        }
+    }, modifier = modifier.border(1.dp, MaterialTheme.colorScheme.background),
+        shape = RectangleShape) {
+        Text(measurementTitles[route] ?: "", textAlign = TextAlign.Center)
+    }
+}
 
-    LazyColumn {
-        items(gridItems) { rowItems ->
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                for (item in rowItems) {
-                    val modifier = Modifier
-                        .height(75.dp)
-                        .width(100.dp)
-                        .weight(1f)
-                        .padding(10.dp, 10.dp)
-
-                    Button(
-                        onClick = {
-                            navController.navigate(item.dst) {
-                                popUpTo(popUpToId)
-                            }
-                        },
-                        modifier = modifier,
-                        shape = androidx.compose.ui.graphics.RectangleShape
-                    ) {
-                        Text(text = item.buttonName)
+@Composable
+fun MeasurementScreen(navController: NavController) {
+    val key = measurementTitles.keys.iterator()
+    val columns = if (LocalConfiguration.current.orientation == Configuration.ORIENTATION_PORTRAIT) 3 else 4
+    LazyColumn(Modifier.border(2.dp, MaterialTheme.colorScheme.background)) {
+        item {
+            while(key.hasNext()) {
+                Row(modifier = Modifier.height(IntrinsicSize.Min)) {
+                    repeat(columns) {
+                        if (key.hasNext())
+                            NaviButton(navController, key.next(),
+                                Modifier
+                                    .weight(1.0f)
+                                    .fillMaxWidth()
+                                    .padding(8.dp))
+                        else
+                            Spacer(Modifier.weight(1.0f))
                     }
                 }
             }

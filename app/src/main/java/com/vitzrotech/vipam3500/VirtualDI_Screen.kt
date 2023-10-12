ac931d72cd4d67b10a6dd2b526317da85cd50623
Cabbage-shaved-ice <142379337+ice-cabbage@users.noreply.github.com>
2023-10-12 13:19:53 +0900

    edit code
    clean

--- app/src/main/java/com/vitzrotech/vipam3500/VirtualDI_Screen.kt
+++ app/src/main/java/com/vitzrotech/vipam3500/VirtualDI_Screen.kt
@@ -1,6 +1,5 @@
 package com.vitzrotech.vipam3500
 
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,7 +11,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.vitzrotech.vipam3500.ui.theme.VIPAM3500Theme
 import androidx.compose.foundation.lazy.items

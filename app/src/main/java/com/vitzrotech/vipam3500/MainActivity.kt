4698ec4677ae09d6c2a6c149990bad23f2bef9c6
Cabbage-shaved-ice <142379337+ice-cabbage@users.noreply.github.com>
2023-10-12 13:18:23 +0900

    edit code
    clean

--- app/src/main/java/com/vitzrotech/vipam3500/MainActivity.kt
+++ app/src/main/java/com/vitzrotech/vipam3500/MainActivity.kt
@@ -1,13 +1,11 @@
 package com.vitzrotech.vipam3500
 
 import android.content.Context
-import android.content.res.Configuration.ORIENTATION_PORTRAIT
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import android.widget.SeekBar
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -36,7 +34,6 @@ import androidx.compose.material.icons.rounded.Menu
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -51,15 +48,10 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -76,16 +68,13 @@ import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
 import com.vitzrotech.vipam3500.ui.theme.VIPAM3500Theme
 import kotlinx.coroutines.launch
-import android.widget.TextView
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setContent {
             VIPAM3500Theme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
@@ -116,7 +105,6 @@ class MainActivity : ComponentActivity() {
         } else {
             val activeNetwork = cm.activeNetworkInfo
             if (activeNetwork != null) {
-                // connected to the internet
                 result = when (activeNetwork.type) {
                     ConnectivityManager.TYPE_WIFI,
                     ConnectivityManager.TYPE_MOBILE,
@@ -128,8 +116,6 @@ class MainActivity : ComponentActivity() {
         return result
     }
 }
-
-@OptIn(ExperimentalMaterial3Api::class)
 
 fun NavGraphBuilder.mainGraph(navController: NavHostController, viewModel: SharedViewModel) {
     navigation(startDestination = "home", route = NavRoutes.MainRoute.name) {
